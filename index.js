3945035e84ded7ec171ff9be1427e4e0f864e2a2
Vojta Jina <vojta.jina@gmail.com>
2014-09-15 16:33:19 -0700

    Rewrite gulp-traceur ;-)
    Assumes the file renaming happens before gulp-traceur.
    
    This works with Traceur 0.0.66+ which significantly changed the API.

--- index.js
+++ index.js
@@ -2,54 +2,53 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var traceur = require('traceur');
-var applySourceMap = require('vinyl-sourcemaps-apply');
-var objectAssign = require('object-assign');
+var path = require('path');
+
+
+function cloneFile(file, override) {
+  var File = file.constructor;
+
+  return new File({
+  	path: override.path || file.path,
+  	cwd: override.cwd || file.cwd,
+  	contents: new Buffer(override.contents || file.contents),
+  	base: override.base || file.base
+  });
+}
+
 
 module.exports = function (options) {
 	options = options || {};
 
-	return through.obj(function (file, enc, cb) {
+	return through.obj(function (file, enc, done) {
 		if (file.isNull()) {
-			cb(null, file);
+			done(null, file);
 			return;
 		}
 
 		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-traceur', 'Streaming not supported'));
+			done(new gutil.PluginError('gulp-traceur', 'Streaming not supported'));
 			return;
 		}
 
-		var ret;
-
-		var fileOptions = objectAssign({}, options);
-		fileOptions.filename = file.relative;
-
-		if (file.sourceMap) {
-			fileOptions.sourceMaps = true;
-		}
+		var compiler = new traceur.Compiler(options);
+		var originalFilePath = file.history[0];
+		var inputFilename = path.relative(file.base, originalFilePath);
+		var outputFilename = file.path;
+		var sourceRoot = path.relative(path.dirname(outputFilename), file.base);
 
 		try {
-			ret = traceur.compile(file.contents.toString(), fileOptions);
+			var transpiledContent = compiler.compile(file.contents.toString(), inputFilename, outputFilename, sourceRoot);
+			var sourceMapString = compiler.getSourceMap();
 
-			if (ret.js) {
-				file.contents = new Buffer(ret.js);
-			}
+		  if (sourceMapString) {
+		    this.push(cloneFile(file, {contents: sourceMapString, path: file.path.replace(/\.js$/, '.map')}));
+		  }
 
-			if (ret.generatedSourceMap && file.sourceMap) {
-				applySourceMap(file, ret.generatedSourceMap);
-			}
-
-			if (ret.errors.length > 0) {
-				cb(new gutil.PluginError('gulp-traceur', '\n' + ret.errors.join('\n'), {
-					fileName: file.path,
-					showStack: false
-				}));
-			} else {
-				cb(null, file);
-			}
+		  done(null, cloneFile(file, {contents: transpiledContent}));
 		} catch (err) {
-			cb(new gutil.PluginError('gulp-traceur', err, {
-				fileName: file.path
+			done(new gutil.PluginError('gulp-traceur', err, {
+				fileName: originalFilePath
 			}));
 		}
 	});
